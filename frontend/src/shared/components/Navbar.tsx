39b10965d584167660c4685eab41bfd8020a06d4
diogo.vasconcelos <diogo.am.vasconcelos@gmail.com>
2021-04-24 07:16:45 +0200

    Improved navbar

--- frontend/src/shared/components/Navbar.tsx
+++ frontend/src/shared/components/Navbar.tsx
@@ -1,24 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Input, Layout, Image } from "antd";
-import logo from "../../../assets/logo.jpg";
+import { Button, Layout } from "antd";
+import logo from "../../../assets/logo-navbar.jpg";
+import styled from "styled-components";
 
-const { Search } = Input;
 const { Header } = Layout;
+
+const NavbarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const LogoContainer = styled.div`
+  flex-grow: 1;
+`;
+
+const ButtonsContainer = styled.div``;
+
+const NavButton = styled(Button)`
+  color: white;
+  :hover {
+    color: gray;
+  }
+`;
 
 export const Navbar: React.FC = () => {
   return (
-    <Layout className="layout">
-      <Header>
-        <div>
+    <Header>
+      <NavbarContainer>
+        <LogoContainer>
           <Link to="/">
             <img src={logo} alt="Social Watcher logo" />
           </Link>
-        </div>
-        <Button type="primary" onClick={() => console.log("Log in")}>
-          Log in
-        </Button>
-      </Header>
-    </Layout>
+        </LogoContainer>
+        <ButtonsContainer>
+          <NavButton type="text" onClick={() => console.log("Log in")}>
+            Log in
+          </NavButton>
+          <NavButton type="primary" onClick={() => console.log("sign up")}>
+            Start free trial
+          </NavButton>
+        </ButtonsContainer>
+      </NavbarContainer>
+    </Header>
   );
 };
