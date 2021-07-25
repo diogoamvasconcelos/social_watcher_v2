0942a3b7b42fcf3db51abd54e2fc748b221a2d22
diogo.vasconcelos <diogo.am.vasconcelos@gmail.com>
2021-07-25 13:13:52 +0200

    Update nixpkgs to get latest node LTS version (v14.17.0)

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { }:
 let
   nixpkgs = import (builtins.fetchTarball {
-    name = "nixpkgs-stable-release-20.09";
-    url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
-    # Hash obtained using nix-prefetch-url --unpack <url>
-    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
+    name = "nixpkgs-stable-release-21.05";
+    url = "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
   }) { };
 in nixpkgs.mkShell {
   buildInputs = with nixpkgs; [
