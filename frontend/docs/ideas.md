bf8eaed65d97a4cb120926ba730af3167ac40450
diogo.vasconcelos <diogo.am.vasconcelos@gmail.com>
2021-08-11 10:55:38 +0200

    Split sider into it's own component

--- frontend/docs/ideas.md
+++ frontend/docs/ideas.md
@@ -1,3 +1,8 @@
 - docs like these: https://reactrouter.com/web/api/Switch
 - landing page images: https://blog.roastmylandingpage.com/landing-page-image/
   - https://encharge.io looks nice
+- ref for dashboards:
+  - https://preview.pro.ant.design/
+  - code:
+    - https://github.com/ant-design/v2.preview.pro.ant.design
+    - https://github.com/ant-design/ant-design-pro-layout/blob/master/src/SiderMenu/SiderMenu.tsx
--- frontend/src/pages/dashboard_v2/DashboardPageV2.tsx
+++ frontend/src/pages/dashboard_v2/DashboardPageV2.tsx
@@ -5,52 +5,81 @@ import Menu from "antd/lib/menu";
 import TagsFilled from "@ant-design/icons/lib/icons/TagsFilled";
 import FileSearchOutlined from "@ant-design/icons/lib/icons/FileSearchOutlined";
 import { MenuClickEventHandler } from "rc-menu/lib/interface";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { KeywordsPage } from "./keywords/KeywordsPage";
 import { SearchPage } from "./search/SearchPage";
+import { useLocationPathChanged } from "../../shared/lib/react";
+import _findKey from "lodash/findKey";
+import { mapRecord } from "../../shared/lib/collections";
 
 // ref: https://preview.pro.ant.design/dashboard/analysis/
 
-const navigationConfig: Record<string, string> = {
-  keywords: "/user/dashboardv2/keywords",
-  search: "/user/dashboardv2/search",
+const navigationConfig: Record<
+  string,
+  { path: string; label: string; icon: React.ReactNode }
+> = {
+  keywords: {
+    path: "/user/dashboardv2/keywords",
+    label: "Keywords",
+    icon: <TagsFilled />,
+  },
+  search: {
+    path: "/user/dashboardv2/search",
+    label: "Search",
+    icon: <FileSearchOutlined />,
+  },
+};
+
+const SideBar: React.FC = () => {
+  const history = useHistory();
+
+  const [collapsed, setCollapsed] = useState(false);
+  const [selectedKeys, setselectedKeys] = useState<string[]>([]);
+
+  const onCollapse = (collapsed: boolean) => {
+    setCollapsed(collapsed);
+  };
+
+  const handleClick: MenuClickEventHandler = ({ key }) => {
+    history.push(navigationConfig[key.toString()].path);
+  };
+
+  useLocationPathChanged((newPath: string) => {
+    const currentKey = _findKey(
+      navigationConfig,
+      (item) => newPath == item.path
+    );
+    setselectedKeys(currentKey ? [currentKey] : []);
+  });
+
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={onCollapse}
+      style={{ minHeight: "100vh" /*span 100% of the viewport*/ }}
+    >
+      <Menu mode="inline" onClick={handleClick} selectedKeys={selectedKeys}>
+        {mapRecord(navigationConfig, (key, value) => (
+          <Menu.Item key={key} icon={value.icon}>
+            {value.label}
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Sider>
+  );
 };
 
 export const DashboardPageV2: React.FC = () => {
-  const history = useHistory();
-
-  const [siderCollapsed, setSiderCollapsed] = useState(false);
-  const [selectedKeys, setselectedKeys] = useState<string[]>([]);
-
-  const onSiderCollapse = (collapsed: boolean) => {
-    setSiderCollapsed(collapsed);
-  };
-  const handleClick: MenuClickEventHandler = ({ key }) => {
-    setselectedKeys([key.toString()]);
-    history.push(navigationConfig[key.toString()]);
-  };
-
   return (
     <Layout>
-      <Sider
-        collapsible
-        collapsed={siderCollapsed}
-        onCollapse={onSiderCollapse}
-        style={{ minHeight: "100vh" }}
-      >
-        <Menu mode="inline" onClick={handleClick} selectedKeys={selectedKeys}>
-          <Menu.Item key="keywords" icon={<TagsFilled />}>
-            Keywords
-          </Menu.Item>
-          <Menu.Item key="search" icon={<FileSearchOutlined />}>
-            Search
-          </Menu.Item>
-        </Menu>
-      </Sider>
+      <SideBar />
       <Switch>
-        <Route path="/" exact>
-          <p>Work in Progress</p>
-        </Route>
+        <Route
+          path="/user/dashboardv2"
+          exact
+          render={() => <Redirect to="/user/dashboardv2/keywords" />}
+        ></Route>
         <Route path="/user/dashboardv2/keywords" component={KeywordsPage} />
         <Route path="/user/dashboardv2/search" component={SearchPage} />
       </Switch>
